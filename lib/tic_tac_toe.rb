ae40525abaa47c850e03c3ab02302f202a056a18
Joshua Needham <joshuabneedham@gmail.com>
2017-10-13 00:08:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,4 +78,41 @@ class TicTacToe
     end
     move(index, player_token)
   end
+  def won?
+    WIN_COMBINATIONS.detect do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = @board[win_index_1] # load the value of the board at win_index_1
+      position_2 = @board[win_index_2] # load the value of the board at win_index_2
+      position_3 = @board[win_index_3] # load the value of the board at win_index_3
+
+      position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+      position_1 == "O" && position_2 == "O" && position_3 == "O"
+    end
+  end
+
+  def full?
+    #This is checking to see if there is an x or o in all the spaces on the board
+      @board.all? do |move|
+        move == "X" || move == "O"
+      end
+  end
+
+  def draw?
+    #If full board and not won
+    full? && !won?
+  end
+
+  def over?(board)
+    draw?(board) || won?(board)
+  end
+
+  def winner(board)
+    if won?(board)
+       win_index_1 = won?(board)[0]
+       return board[win_index_1]
+    end
+  end
 end
