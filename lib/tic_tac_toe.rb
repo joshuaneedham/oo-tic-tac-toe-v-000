63664e5f4c0396b2e107e9dc09c9ab6488d242dd
Joshua Needham <joshuabneedham@gmail.com>
2017-10-13 00:11:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class TicTacToe
   def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = Array.new(9, " ")
   end
   WIN_COMBINATIONS = [
   [0,1,2],
